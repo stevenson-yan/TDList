f0b0d75044b3bc89039aabf07fe4e7d01d5bb765
Stevenson Yan <styan656@students.cs.ubc.ca>
2024-03-17 18:36:27 -0700

    Initial Commit

--- App.js
+++ App.js
@@ -1,10 +1,20 @@
 import { StatusBar } from 'expo-status-bar';
+import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <View style={styles.header}>
+        <Text style={styles.boldText}>
+          Hello, World!
+        </Text>
+      </View>
+      <View style={styles.body}>
+        <Text>
+          Lorem ipsem dolor sit amet.
+        </Text>
+      </View>
       <StatusBar style="auto" />
     </View>
   );
@@ -17,4 +27,15 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  header: {
+    backgroundColor: 'pink',
+    padding: 20,
+  },
+  boldText: {
+    fontWeight: 'bold',
+  },
+  body: {
+    backgroundColor: 'yellow',
+    padding: 20,
+  }
 });
